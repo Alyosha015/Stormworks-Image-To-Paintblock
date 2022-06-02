0b862ac4666b471b5b718144d92e2d25cf4de383
Alyosha15 <99307745+Alyosha015@users.noreply.github.com>
2022-06-02 19:22:44 -0400

    Delete Settings.cs

--- Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ImageToPaintblockConverter {
-    class Settings {
-        public static String settingsFileName = "ImageToPaintblockConverterSettings.txt";
-        public static String vehicleName = "";
-        public static String vehicleFilePath = "";
-    }
-}
